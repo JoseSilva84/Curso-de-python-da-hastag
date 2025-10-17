5c9701bd38e80d03f1ae3f0528534232307ebaef
José Evangelista da Silva Filho <juniornyanata@gmail.com>
2025-10-17 08:08:54 -0300

    mudanca2

---
+++ Primeiro projeto python.py
@@ -0,0 +1,7 @@
+primeiro_valor = input("Digite o primeiro valor: ")
+segundo_valor = input("Digite o segundo valor: ")
+
+if int(primeiro_valor) > int(segundo_valor):
+        print("O primeiro valor e maior!")
+else:
+        print("O segundo valor é maior!")
